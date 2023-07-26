342bacbbc94c6c6518a0cf636d957bd0da188327
gokhanagkurt <goekhan.agkurt@outlook.de>
2023-07-26 20:19:48 +0200

    Add Frontend

--- backend/src/main/java/com/example/backend/model/Gif.java
+++ backend/src/main/java/com/example/backend/model/Gif.java
@@ -1,6 +1,4 @@
 package com.example.backend.model;
-
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
---
+++ frontend/src/models.ts
@@ -0,0 +1,13 @@
+export type Gif = {
+    id: string,
+    name: string,
+    description: string,
+    price: string
+}
+
+export type GifWithoutId = {
+    id: string,
+    name: string,
+    description: string,
+    price: string
+}
