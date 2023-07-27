b81b366d84ce6505997481bf36bff03707242186
gokhanagkurt <goekhan.agkurt@outlook.de>
2023-07-27 18:25:17 +0200

    add UuIdService

---
+++ backend/src/main/java/com/example/backend/service/UuIdService.java
@@ -0,0 +1,15 @@
+package com.example.backend.service;
+
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@Service
+public class UuIdService {
+    public String getRandomId() {
+        return UUID.randomUUID().toString();
+    }
+}
